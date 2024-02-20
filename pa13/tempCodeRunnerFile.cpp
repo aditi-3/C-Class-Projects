f13de6f3386e20a1c30008b96e6a7ca5b65f5f66
aditi-3 <75766710+aditi-3@users.noreply.github.com>
2024-02-19 22:33:16 -0800

    Delete pa13 directory

--- pa13/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-if(openAcc == "y")
-                    // {
-                    //     break; break;
-                    // }
-                    // if(openAcc == "n")
-                    // {
-                    //     choice = "3";
-                    //     break; break;
-                    // }
