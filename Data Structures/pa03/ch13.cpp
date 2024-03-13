154eafb6f34cf5d4c29de97f79e07be2e8b701f7
Aditi <aditi.verma.av19@gmail.com>
2024-03-13 15:31:08 -0700

    .

--- Data Structures/pa03/ch13.cpp
+++ Data Structures/pa03/ch13.cpp
@@ -11,28 +11,79 @@ using namespace std;
 void printMatrix(int *a, int len);
 void findInteger(int *a, int len);
 void swapCol(int *a, int len);
+class dogInventory
+{
+    private:
+        string dogLicense; //<10chars
+        string dogName; //<15 chars
+        string ownerPhone; //<11 chars
+        float priceQuoted; //>= 0 price for bath
+
+    public:
+        dogInventory()
+        {
+
+        }
+
+        //getters
+        string getDogLicense()
+        {
+            return dogLicense;
+        }
+
+        string getGodName()
+        {
+            return dogName;
+        }
+
+        string getOwnerPhone()
+        {
+            return ownerPhone;
+        }
+
+        float getPriceQuoted()
+        {
+            return priceQuoted;
+        }
+
+        //setters
+        void setDogLicense(string li)
+        {
+            dogLicense = li;
+        }
+
+        void setGodName(string name)
+        {
+            dogName = name;
+        }
+
+        void setOwnerPhone(string phone)
+        {
+            ownerPhone = phone;
+        }
+
+        void setPriceQuoted(float price)
+        {
+            priceQuoted = price;
+        }
+};
 
 int main()
 {
     // Initialize array and determine it's number of rows and columns
-    int array[] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31};
     int input = 0;
-    int *point;
-    point = array;
-    int l = sizeof array / sizeof *point;
 
     // Excecute menu until user exits
     do{
         // User input for menu
         cout<<"\n\n----------MENU----------";
-        cout<<"\n1). Display matrix";
-        cout<<"\n2). Find integer";
-        cout<<"\n3). Swap column 1 and 3";
-        cout<<"\n4). Exit";
-        cout<<"\nEnter your value: ";
+        cout<<"\n1). Enter a new job";
+        cout<<"\n2). List all current jobs";
+        cout<<"\n3). Exit";
+        cout<<"\nEnter menu choice: ";
         cin>>input;
 
-        if (input < 1 || input > 4)
+        if (input < 1 || input > 3)
         {
             cout<<"\nWrong input, try again!";
         }
@@ -40,19 +91,15 @@ int main()
         switch(input)
         {
             case 1: 
-                printMatrix(point, l);
                 break;
             case 2:
-                findInteger(point, l);
                 break;
             case 3:
-                swapCol(point, l);
-                break;
-            case 4:
                 cout<<"Thank you!";
+                break;
         }
     }
-    while (input != 4); 
+    while (input != 3); 
 }
 
 /** FUNCTION
